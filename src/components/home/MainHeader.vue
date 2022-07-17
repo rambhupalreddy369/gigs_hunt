0e6b6d33207ebccfafaf15fc69be6ad00dc23f4f
rambhupal <rambhupalreddy369@gmail.com>
2022-07-17 13:33:38 +0530

    project set up

---
+++ src/components/home/MainHeader.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		hello world
+	</div>
+	<section class="w-full h-980">
+		<nav class="w-full h-16 bg-black flex items-center justify-between">
+			<div class="w-full container flex items-center justify-between mx-auto">
+				<div class="pb-4 text-white">
+					<img src="../../assets/header/title.png" />
+				</div>
+				<div class="image"><img src="../../assets/header/download.png" /></div>
+			</div>
+		</nav>
+		<section class="columns-2 bg-black text-white">
+			<div class="w-full txt text-center justify-center">
+				<h1>Hunt for gigs</h1>
+				<p>
+					Find jobs at your area in agriculture, construction, warehouses,
+					delivery and many related places
+				</p>
+				<p>Download the gigshunt app in the playstore</p>
+				<img src="../../assets/header/download.png" />
+			</div>
+			<div class="w-full image text-center justify-center">
+				<img src="../../assets/home/image1.png" />
+			</div>
+		</section>
+	</section>
+</template>
+<script></script>
+
+<style scoped></style>
